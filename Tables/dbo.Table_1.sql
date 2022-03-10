c9d781e3c0b736a5eac6eb421cbeed6e5a893dff
klacol <klaus.aengenvoort@etask.de>
2022-03-10 07:32:40 +0100

    My first database

---
+++ Tables/dbo.Table_1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Table_1]
+(
+[C1] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[C2] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[C3] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
+) ON [PRIMARY]
+GO
