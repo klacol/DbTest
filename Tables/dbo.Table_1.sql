a7d99d7b642809796e3ed5de8fd240cc343223bb
klacol <klaus.aengenvoort@etask.de>
2022-03-10 07:34:22 +0100

    Zwei neue Spalten im Feature-Branch

--- Tables/dbo.Table_1.sql
+++ Tables/dbo.Table_1.sql
@@ -2,6 +2,8 @@ CREATE TABLE [dbo].[Table_1]
 (
 [C1] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
 [C2] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
-[C3] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
+[C3] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[C4] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[C5] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
